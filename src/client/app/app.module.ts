90f587e394c77911ea5721d7d7705c41b0fdd84a
Akito Hataguchi <hataguchi@motionpicture.jp>
2019-09-10 18:32:12 +0900

    ### Changed  - Differential Loading適用

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -5,6 +5,7 @@
 import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { BsDatepickerModule, ModalModule, PaginationModule } from 'ngx-bootstrap';
@@ -25,6 +26,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     ],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         StoreModule,
         CoreStoreModule,
         SharedModule,
