c2b139b3098239812f0867ac00eb0193014d604d
hataguchi <hataguchi@motionpicture.jp>
2021-07-01 16:35:27 +0900

    ### Fixed  - QR非表示でも発券される不具合修正

--- src/client/app/modules/shared/components/parts/item-list/item-list.component.ts
+++ src/client/app/modules/shared/components/parts/item-list/item-list.component.ts
@@ -6,45 +6,53 @@ import { getEnvironment } from '../../../../../../environments/environment';
 @Component({
     selector: 'app-item-list',
     templateUrl: './item-list.component.html',
-    styleUrls: ['./item-list.component.scss']
+    styleUrls: ['./item-list.component.scss'],
 })
 export class ItemListComponent implements OnInit {
     @Input() public reservations?: Models.Purchase.Reservation.IReservation[];
-    @Input() public authorizeSeatReservations?: factory.chevre.reservation.IReservation<factory.chevre.reservationType.EventReservation>[];
-    @Input() public acceptedOffers?: factory.order.IAcceptedOffer<factory.order.IItemOffered>[];
+    @Input()
+    public authorizeSeatReservations?: factory.chevre.reservation.IReservation<factory.chevre.reservationType.EventReservation>[];
+    @Input()
+    public acceptedOffers?: factory.order.IAcceptedOffer<factory.order.IItemOffered>[];
     @Input() public order?: factory.order.IOrder;
     @Input() public code?: string;
     @Output() public openQrcode = new EventEmitter<{ id: string }>();
     public environment = getEnvironment();
-    public isShowQRCode = Functions.Order.isShowQRCode;
     public createQRCode = Functions.Order.createQRCode;
 
+    constructor() {}
 
-    constructor() { }
-
-    public ngOnInit() {
-    }
+    public ngOnInit() {}
 
     /**
      * 一時予約から価格取得
      */
-    public getReservationPriceComponents(reservation: Models.Purchase.Reservation.IReservation) {
-        const priceComponents:
-            factory.chevre.priceSpecification.IPriceSpecification<factory.chevre.priceSpecificationType>[] = [];
-        if (reservation.seat !== undefined && reservation.seat.offers !== undefined) {
+    public getReservationPriceComponents(
+        reservation: Models.Purchase.Reservation.IReservation
+    ) {
+        const priceComponents: factory.chevre.priceSpecification.IPriceSpecification<factory.chevre.priceSpecificationType>[] =
+            [];
+        if (
+            reservation.seat !== undefined &&
+            reservation.seat.offers !== undefined
+        ) {
             // 座席料金
             reservation.seat.offers.forEach((o) => {
                 if (o.priceSpecification !== undefined) {
-                    o.priceSpecification.priceComponent.forEach(p => priceComponents.push(p));
+                    o.priceSpecification.priceComponent.forEach((p) =>
+                        priceComponents.push(p)
+                    );
                 }
             });
         }
         if (reservation.ticket !== undefined) {
             // 券種料金
-            reservation.ticket.ticketOffer.priceSpecification.priceComponent.forEach(p => priceComponents.push(p));
+            reservation.ticket.ticketOffer.priceSpecification.priceComponent.forEach(
+                (p) => priceComponents.push(p)
+            );
             if (reservation.ticket.addOn !== undefined) {
                 // 券種オプション料金
-                reservation.ticket.addOn.forEach(a => {
+                reservation.ticket.addOn.forEach((a) => {
                     if (a.priceSpecification === undefined) {
                         return;
                     }
@@ -60,77 +68,85 @@ export class ItemListComponent implements OnInit {
      */
     public changeTicketCount() {
         const priceComponents: {
-            name: { ja?: string; en?: string; };
+            name: { ja?: string; en?: string };
             price: number;
             priceCurrency: factory.chevre.priceCurrency;
-            referenceQuantity: factory.chevre.quantitativeValue.IQuantitativeValue<factory.chevre.unitCode>
+            referenceQuantity: factory.chevre.quantitativeValue.IQuantitativeValue<factory.chevre.unitCode>;
         }[] = [];
         if (this.reservations !== undefined) {
-            this.reservations.forEach(r => {
+            this.reservations.forEach((r) => {
                 this.getReservationPriceComponents(r).forEach((p) => {
-                    if (p.name === undefined
-                        || typeof p.name === 'string'
-                        || p.price === undefined) {
+                    if (
+                        p.name === undefined ||
+                        typeof p.name === 'string' ||
+                        p.price === undefined
+                    ) {
                         return;
                     }
                     priceComponents.push({
                         name: p.name,
                         price: p.price,
                         priceCurrency: p.priceCurrency,
-                        referenceQuantity: (<any>p).referenceQuantity
+                        referenceQuantity: (<any>p).referenceQuantity,
                     });
                 });
             });
         } else if (this.authorizeSeatReservations !== undefined) {
-            this.authorizeSeatReservations.forEach(r => {
-                if (r.price === undefined || typeof (r.price) === 'number') {
+            this.authorizeSeatReservations.forEach((r) => {
+                if (r.price === undefined || typeof r.price === 'number') {
                     return;
                 }
-                r.price.priceComponent.forEach(p => {
-                    if (p.name === undefined
-                        || typeof p.name === 'string'
-                        || p.price === undefined) {
+                r.price.priceComponent.forEach((p) => {
+                    if (
+                        p.name === undefined ||
+                        typeof p.name === 'string' ||
+                        p.price === undefined
+                    ) {
                         return;
                     }
                     priceComponents.push({
                         name: p.name,
                         price: p.price,
                         priceCurrency: p.priceCurrency,
-                        referenceQuantity: (<any>p).referenceQuantity
+                        referenceQuantity: (<any>p).referenceQuantity,
                     });
                 });
             });
         } else if (this.acceptedOffers !== undefined) {
-            this.acceptedOffers.forEach(o => {
+            this.acceptedOffers.forEach((o) => {
                 if (o.priceSpecification === undefined) {
                     return;
                 }
                 (<any>o.priceSpecification).priceComponent.forEach((p: any) => {
-                    if (p.name === undefined
-                        || typeof p.name === 'string'
-                        || p.price === undefined) {
+                    if (
+                        p.name === undefined ||
+                        typeof p.name === 'string' ||
+                        p.price === undefined
+                    ) {
                         return;
                     }
                     priceComponents.push({
                         name: p.name,
                         price: p.price,
                         priceCurrency: p.priceCurrency,
-                        referenceQuantity: (<any>p).referenceQuantity
+                        referenceQuantity: (<any>p).referenceQuantity,
                     });
                 });
             });
         }
         const result: {
             priceComponent: {
-                name: { ja?: string; en?: string; };
+                name: { ja?: string; en?: string };
                 price: number;
                 priceCurrency: factory.chevre.priceCurrency;
-                referenceQuantity: factory.chevre.quantitativeValue.IQuantitativeValue<factory.chevre.unitCode>
+                referenceQuantity: factory.chevre.quantitativeValue.IQuantitativeValue<factory.chevre.unitCode>;
             };
             count: number;
         }[] = [];
-        priceComponents.forEach(p => {
-            const findResult = result.find(r => JSON.stringify(r.priceComponent) === JSON.stringify(p));
+        priceComponents.forEach((p) => {
+            const findResult = result.find(
+                (r) => JSON.stringify(r.priceComponent) === JSON.stringify(p)
+            );
             if (findResult === undefined) {
                 result.push({ priceComponent: p, count: 1 });
                 return;
@@ -139,5 +155,4 @@ export class ItemListComponent implements OnInit {
         });
         return result;
     }
-
 }
